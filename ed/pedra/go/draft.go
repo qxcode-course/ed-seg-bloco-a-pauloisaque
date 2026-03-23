c5d79d9deb159ced83e1e9e3ea0cd72f1b01a4a1
pauloisaque <pauloisaquecontatocomercial@gmail.com>
2026-03-22 22:32:50 -0300

    fiz mais um pouco hoje à noite. Usei IA principalmente para entender o problema e buscar alternativas, quando não sabia apenas.

--- ed/pedra/go/draft.go
+++ ed/pedra/go/draft.go
@@ -13,16 +13,34 @@ func main() {
 	for i := range jogadas {
 		fmt.Scan(&jogadas[i].pa, &jogadas[i].pb)
 	}
-	fmt.Println(jogar(jogadas))
+	var resultadoJogadas = jogar(jogadas)
+	if resultadoJogadas == 0 {
+		fmt.Println("sem ganhador")
+	} else {
+		fmt.Println(jogar(jogadas))
+	}
 }
 
 func jogar(jogadas []Jogada) int32 {
 	var placarFinal [][]int32
-	fmt.Println(placarFinal)
+	var vencedor int32
+
 	for i := range jogadas {
+		if jogadas[i].pa < 10 || jogadas[i].pb < 10 {
+			placarFinal = append(placarFinal, []int32{100, int32(i)})
+			continue
+		}
 		var resultadoJogada []int32
 		resultadoJogada = append(resultadoJogada, int32(jogadas[i].pb-jogadas[i].pa), int32(i))
 		placarFinal = append(placarFinal, resultadoJogada)
 	}
-	return 0
+
+	var minimo int32 = placarFinal[0][0]
+	for _, v := range placarFinal {
+		if v[0] < minimo {
+			minimo = v[0]
+			vencedor = v[1]
+		}
+	}
+	return vencedor
 }
