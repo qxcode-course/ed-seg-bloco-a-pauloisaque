b75a739118f6586c886527ed3348a494f3ea6637
Paulo Isaque <pauloisaquecontatocomercial@gmail.com>
2026-03-27 16:20:13 +0000

    Hoje fiz muitas coisas no Almoço com Código. Não consegui ir ontem pois me atrasei no onibus e não cheguei a tempo. Fiquei bem preso na atividade da princesa.

--- ed/princesa/go/draft.go
+++ ed/princesa/go/draft.go
@@ -2,18 +2,35 @@ package main
 
 import "fmt"
 
+type vivo struct {
+    num int32
+    taVivo bool
+}
+
 func main() {
 	var pessoas int32
 	var espada int32
 	fmt.Scan(&pessoas, &espada)
 
-	var vivos []int32
+	var vivos = make([]vivo, pessoas)
 	for i := int32(0); i < pessoas; i++ {
-		vivos = append(vivos, i+1)
+		vivos[i].num = i + 1
+        vivos[i].taVivo = true
 	}
 
-	for i := range vivos {
-		fmt.Print(vivos[i])
-	}
+    fmt.Print(knext(vivos, espada))
 
+}
+
+func knext (vivos []vivo, espada int32) string {
+    for i := range vivos {
+        if i > len(vivos) {
+            i = 0
+        }
+        if vivos[espada].taVivo == true {
+            vivos[i + 1].taVivo = false
+        } 
+    }
+    
+    
 }
