0812204056f3433f1f47c1fb4be74a45409e74e3
Paulo Isaque <pauloisaquecontatocomercial@gmail.com>
2026-04-07 20:05:33 +0000

    Fiz um pouco dos fractais, tentando instalar novamente e entender

--- ed/fractais_png/src/go/main.go
+++ ed/fractais_png/src/go/main.go
@@ -10,15 +10,15 @@ func randInt(min, max int) int {
 }
 
 func main() {
-	pen := NewPen(500, 500)   // cria um canvas de 500 de largura por 500 de altura
-	pen.SetRGB(255, 0, 0)     // muda a cor do pincel para vermelho
+	pen := NewPen(500, 500)      // cria um canvas de 500 de largura por 500 de altura
+	pen.SetRGB(0, 0, 0)    // muda a cor do pincel para vermelho
 	pen.SetPosition(250, 500) // move o pincel para x 250, y 500
-	pen.SetHeading(90)        // coloca o pincel apontando para cima
-	pen.Walk(100)             // anda 100 pixels
-	pen.Left(30)              // dobra 30 graus para esquerda
-	pen.Walk(100)             // anda 100 pixels
-	pen.DrawCircle(50)        // desenha um círculo de raio 50
-	pen.Right(60)             // gira para direita 60 graus
+	pen.SetHeading(90)       // coloca o pincel apontando para cima
+	pen.Walk(100)            // anda 100 pixels
+	pen.Left(30)             // dobra 30 graus para esquerda
+	pen.Walk(100)            // anda 100 pixels
+	pen.DrawCircle(50)       // desenha um círculo de raio 50
+	pen.Right(60)            // gira para direita 60 graus
 	pen.Walk(150)
 	for range 10 {
 		pen.Up()
