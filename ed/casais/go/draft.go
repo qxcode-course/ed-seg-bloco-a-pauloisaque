d121792232e2eda41ba1b0f6863cead1c4395726
Paulo Isaque <pauloisaquecontatocomercial@gmail.com>
2026-05-04 20:06:30 +0000

    atividades em sala de aula

--- ed/casais/go/draft.go
+++ ed/casais/go/draft.go
@@ -1,14 +1,10 @@
 package main
 import "fmt"
 func main() {
-    var e1 int32
-    e1 = fmt.Scan(&e1)
-    var e2 int32[e1]
-    e2 = fmt.Scan(&e2)
-
-    var animal int32
-    var descasados int32[]
-
+    var qnt int
+    fmt.Scan(&qtd)
     
+
+
 
 }
