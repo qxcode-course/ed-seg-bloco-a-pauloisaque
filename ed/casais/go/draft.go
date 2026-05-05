a4929b19f99e3c68b8f0047a55ad545eddf459ac
pauloisaque <pauloisaquecontatocomercial@gmail.com>
2026-05-04 21:46:38 -0300

    Fiz um pouco de Revisão em casa

--- ed/casais/go/draft.go
+++ ed/casais/go/draft.go
@@ -1,10 +1,44 @@
 package main
+
 import "fmt"
+
 func main() {
-    var qnt int
-    fmt.Scan(&qtd)
-    
+	var qnt int
+	fmt.Scan(&qnt)
+	var vetor []int
+	for {
+		fmt.Scan(&vetor)
+	}
 
+	fmt.Print(verificaPares(vetor, qnt))
 
+}
+
+func verificaPares(vetor []int, qnt int) int {
+	novoVetor := append([]int{}, vetor...)
+	pontaUm := 0
+	pontaDois := 0
+	countPares := 0
+
+	iniciarProcesso := func() {
+		pontaUm = 0
+		pontaDois = 0
+	}
+
+	for pontaDois < qnt {
+		if novoVetor[pontaUm] == novoVetor[pontaDois] && pontaUm != pontaDois {
+			novoVetor = append(vetor[:pontaUm], vetor[pontaUm+1:]...)
+			novoVetor = append(vetor[:pontaDois], vetor[pontaDois+1:]...)
+			countPares++
+			iniciarProcesso()
+		} else {
+			if pontaDois > qnt {
+				break
+			}
+			pontaDois++
+		}
+	}
+
+	return countPares
 
 }
