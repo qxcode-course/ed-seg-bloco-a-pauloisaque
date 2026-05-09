aa5bbef39bf5472279798376fed8fa95224922a5
pauloisaque <pauloisaquecontatocomercial@gmail.com>
2026-05-09 08:54:57 -0300

    fazendo um pouco de revisão no sábado em casa

--- ed/baruel/src/go/draft.go
+++ ed/baruel/src/go/draft.go
@@ -6,28 +6,49 @@ func main() {
 	fmt.Println("Hello, World!")
 	var slotsAlbum int
 	var quantasTem int
-	figurinhas := make([]int, quantasTem)
 	fmt.Scan(&slotsAlbum)
 	fmt.Scan(&quantasTem)
+	figurinhas := make([]int, quantasTem)
 	for i := 0; i < quantasTem; i++ {
 		fmt.Scan(&figurinhas[i])
 	}
 
-	fmt.Println(verificarRepetidas(slotsAlbum, quantasTem, figurinhas))
+	verificarRepetidas(slotsAlbum, quantasTem, figurinhas)
 }
 
 func verificarRepetidas(slotsAlbum int, quantasTem int, figurinhas []int) {
-	var deveriaTer []int
+	type slots struct {
+		deviaTer int
+		slot     int
+	}
+	var deveriaTer []slots = make([]slots, slotsAlbum)
 	for i := 0; i < slotsAlbum; i++ {
-		deveriaTer[i] = i + 1
+		deveriaTer[i] = slots{i + 1, 0}
+	}
+	var figurinhasRepetidas []int = make([]int, 0)
+
+	for i := 0; i < quantasTem; i++ {
+		for i := 0; i < len(deveriaTer); i++ {
+			if figurinhas[i] == deveriaTer[i].deviaTer && deveriaTer[i].slot == 0 {
+				deveriaTer[i].slot = figurinhas[i]
+			}
+		}
 	}
 
-}
+	for i := 0; i < quantasTem; i++ {
+		if figurinhas[i] == deveriaTer[i].deviaTer && deveriaTer[i].slot != 0 {
+			figurinhasRepetidas = append(figurinhasRepetidas, figurinhas[i])
+		}
+	}
 
-func verificaRepetidas(slotsAlbum int, quantasTem int, figurinhas []int) []int {
+	var quantasEstaoFaltando int
+	for i := 0; i < len(deveriaTer); i++ {
+		if deveriaTer[i].slot == 0 {
+			quantasEstaoFaltando++
+		}
+	}
 
-}
-
-func verificaFaltantes(slotsAlbum int, quantasTem int, figurinhas []int) []int {
+	fmt.Println(figurinhasRepetidas)
+	fmt.Println(quantasEstaoFaltando)
 
 }
