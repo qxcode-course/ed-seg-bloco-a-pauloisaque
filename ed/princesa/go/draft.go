2edb55e16bb30ca00602b4129768aaac1fec2101
pauloisaque <pauloisaquecontatocomercial@gmail.com>
2026-05-11 20:25:00 -0300

    Fiz mais um pouco hoje de noite

--- ed/princesa/go/draft.go
+++ ed/princesa/go/draft.go
@@ -12,18 +12,43 @@ func main() {
 func josephusProblem(n int, e int) {
 	var vetor []int = make([]int, n)
 	for i := 0; i < n; i++ {
-		vetor[i] = i + 1
+		vetor[i] = 1
 	}
-	fmt.Println(vetor)
 
-	for i := 0; i < len(vetor); i++ {
-		valor := (i + e) % len(vetor)
-		if vetor[valor+1] != 0 {
-			vetor[valor+1] = 0
-			e = (e + 2) % len(vetor)
-		} else {
-			continue
+	proximo := func() int {
+		var res int
+		for i := 0; i < len(vetor); i++ {
+			valorAtual := (i + e) % len(vetor)
+			valorVerificado := (valorAtual + 1) % len(vetor)
+			if vetor[valorVerificado] == 1 {
+				res = valorVerificado
+				break
+			}
 		}
+		return res
 	}
+
+	transformarProximoFalse := func() bool {
+		var proximoTrue int
+		proximoTrue = proximo()
+		vetor[proximoTrue] = 0
+		return false
+	}
+
+	var temUmElemento bool
+	temUmElemento = false
+	for temUmElemento == false {
+		temUmElemento = transformarProximoFalse()
+	}
+
+	fmt.Println(proximo())
+
+}
+
+func transformaProximoEmFalse() {
+
+}
+
+func passarEspadaProximo() {
 
 }
