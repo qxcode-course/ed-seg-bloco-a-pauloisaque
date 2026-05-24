a5990c3ef7ae8cdc4f12e48fbbdee6dc8d710c6e
pauloisaque <pauloisaquecontatocomercial@gmail.com>
2026-05-24 16:42:40 -0300

    fractais difíceis

--- ed/fractais_png/src/go/main.go
+++ ed/fractais_png/src/go/main.go
@@ -9,25 +9,61 @@ func randInt(min, max int) int {
 	return min + rand.Intn(max-min+1)
 }
 
-func embua(pen *Pen, dist float64) {
-	if dist < 1 {
+//arvore.png
+
+// func arvore(pen *Pen, dist float64) {
+// 	if dist < 10 {
+// 		return
+// 	}
+// 	ang := 35.0
+// 	fator := 0.75
+// 	pen.Walk(dist)
+// 	//pen.SavePNG("tree.png")
+// 	//var dummy rune
+// 	//fmt.Scanf("%c", &dummy)
+// 	pen.Right(ang)
+// 	arvore(pen, dist*fator)
+// 	pen.Right(-2 * ang)
+// 	arvore(pen, dist*fator)
+// 	pen.Right(ang)
+// 	pen.Walk(-dist)
+// }
+
+// func main() {
+// 	pen := NewPen(500, 500)
+// 	pen.SetHeading(90)
+// 	pen.SetPosition(250, 500)
+// 	arvore(pen, 100)
+
+// 	pen.SavePNG("arvore.png")
+// 	fmt.Println("PNG file created successfully.")
+// }
+
+func circulos(pen *Pen, dist float64) {
+	if dist < 50 {
 		return
 	}
-	pen.SetLineWidth(dist / 30)
-	pen.Walk(dist)
-	pen.Right(90)
-	dist *= 0.95
-	embua(pen, dist)
+	ang := 300.0
+	fator := 0.75
+	pen.DrawCircle(100)
+	pen.SetHeading(90)
+	//pen.SavePNG("tree.png")
+	//var dummy rune
+	//fmt.Scanf("%c", &dummy)
+	pen.Right(ang)
+	circulos(pen, dist*fator)
+	pen.Right(-2 * ang)
+	circulos(pen, dist*fator)
+	pen.Right(ang)
+	pen.Walk(-dist)
 }
 
 func main() {
 	pen := NewPen(500, 500)
-	pen.SetRGB(0, 0, 250)
+	pen.SetHeading(90)
 	pen.SetPosition(250, 250)
-	pen.SetHeading(0)
-	pen.SetPosition(100, 100)
-	embua(pen, 300)
+	circulos(pen, 100)
 
-	pen.SavePNG("tree.png")
+	pen.SavePNG("circulos.png")
 	fmt.Println("PNG file created successfully.")
 }
