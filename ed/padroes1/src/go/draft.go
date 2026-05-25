64dc14c7730090462cbf36cef87ace43f2d43343
pauloisaque <pauloisaquecontatocomercial@gmail.com>
2026-05-25 14:57:01 -0300

    dei uma olhada

--- ed/padroes1/src/go/draft.go
+++ ed/padroes1/src/go/draft.go
@@ -1,5 +1,16 @@
 package main
+
 import "fmt"
+
 func main() {
-    fmt.Println("Hello, World!")
+	var n int
+	fmt.Scan(&n)
+	fmt.Println(blocks(n))
+}
+
+func blocks(n int) int {
+	if n == 1 {
+		return 20
+	}
+	return blocks(n-1) + 8
 }
