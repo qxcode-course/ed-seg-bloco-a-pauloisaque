006e6d6289e8234b61402eb287bfd307c77aff6e
pauloisaque <pauloisaquecontatocomercial@gmail.com>
2026-05-26 07:48:19 -0300

    atv pela manhã

--- ed/padroes2/src/go/draft.go
+++ ed/padroes2/src/go/draft.go
@@ -1,5 +1,16 @@
 package main
+
 import "fmt"
+
 func main() {
-    fmt.Println("Hello, World!")
+	var n int
+	fmt.Scan(&n)
+	fmt.Println(codigo(n))
+}
+
+func codigo(n int) int {
+	if n == 0 {
+		return 0
+	}
+	return codigo(n-1) + 2*n + 1
 }
--- ed/padroes3/src/go/draft.go
+++ ed/padroes3/src/go/draft.go
@@ -1,13 +1,16 @@
 package main
+
 import "fmt"
+
 func main() {
-    fmt.Scan(&)
-    fmt.Println("Hello, World!")
+	var n, m int
+	fmt.Scan(&n, &m)
+	fmt.Println(calcular(n, m))
 }
 
-func calcular(n int, m int) {
-    if m > 1 {
-        return 1
-    }
-    
+func calcular(n int, m int) int {
+	if m == 1 {
+		return 1
+	}
+	return calcular(n, m-1) + (n-2)*(m-1) + 1
 }
