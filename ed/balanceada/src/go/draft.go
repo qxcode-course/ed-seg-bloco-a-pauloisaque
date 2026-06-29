739a8fd46a896fc69beee3f7e74289f74eae5da9
pauloisaque <pauloisaquecontatocomercial@gmail.com>
2026-06-29 11:31:41 -0300

    falta algumas ainda

--- ed/balanceada/src/go/draft.go
+++ ed/balanceada/src/go/draft.go
@@ -1,15 +1,33 @@
 package main
+
 import "fmt"
+
+type Table struct {
+	parenteses int
+	colchetes  int
+}
+
 func main() {
-    var a string
-    fmt.Scan(&a)
-    for i := 0; i < len(a); i++ {
-        if a[i] == "(" {
-            for j := i; j < len(a); j++ {
-                if a[j] == ")" {
-                    break;
-                }
-            }
-        }
+	var a string
+	fmt.Scan(&a)
+	var tabela Table
+	for i := 0; i < len(a); i++ {
+		if a[i] == '(' {
+			tabela.parenteses++
+		}
+		if a[i] == ')' {
+			tabela.parenteses--
+		}
+		if a[i] == '[' {
+			tabela.colchetes++
+		}
+		if a[i] == ']' {
+			tabela.colchetes--
+		}
+	}
+	if tabela.colchetes == 0 && tabela.parenteses == 0 {
+        fmt.Println("balanceado")
+    } else {
+        fmt.Println("nao balanceado")
     }
 }
--- ed/chaves/src/go/main.go
+++ ed/chaves/src/go/main.go
@@ -1,9 +1,39 @@
 package main
 
-func main() {
-	lista := NewQueue[T]()
-	times := "ABCDEFGHI"
-	for i := 0; i < len(times); i++ {
-		lista.Enqueue(times[i])
+import "fmt"
+
+func (q *Queue[T]) quemGanhou(a int, b int) {
+	for q.items.Len() > 1 {
+		e1 := q.Dequeue()
+		e2 := q.Dequeue()
+		if a > b {
+			q.Enqueue(e1)
+			break
+		}
+		if b > a {
+			q.Enqueue(e2)
+			break
+		}
 	}
+}
+
+func main() {
+	var a, b int
+
+	lista := NewQueue[string]()
+
+	times := "ABCDEFGHIJKLMNOP"
+	for i := 0; i < len(times); i++ {
+		lista.Enqueue(string(times[i]))
+	}
+
+	for i := 0; i < 15; i++ {
+		fmt.Scan(&a, &b)
+		lista.quemGanhou(a, b)
+	}
+
+	fmt.Println(lista.Dequeue())
+
+	
+	
 }
