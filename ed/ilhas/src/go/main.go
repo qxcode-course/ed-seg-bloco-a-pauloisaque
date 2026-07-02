fce7caace58e58a80e32139ad871b04cf755035d
pauloisaque <pauloisaquecontatocomercial@gmail.com>
2026-07-01 22:01:57 -0300

    atividades de ed

---
+++ ed/ilhas/src/go/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+// Não modifique a assinatura da função numIslands
+// Ela é a função que será chamada no LeetCode para resolver o problema
+func numIslands(grid [][]byte) int {
+	if grid == null || len(grid) == 0 || len(grid[0]) {
+		return 0
+	}
+	count := 0
+
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			if grid[i][j] == '1' {
+				dfs(grid, i, j)
+				count++
+			}
+			return count
+		}
+	}
+}
+
+func dfs(char[][] grid, int i, int j) {
+	
+}
+
+// Não modifique a função main
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	line := scanner.Text()
+	var nl, nc int
+	fmt.Sscanf(line, "%d %d", &nl, &nc)
+	grid := make([][]byte, nl)
+	for i := 0; i < nl; i++ {
+		scanner.Scan()
+		grid[i] = []byte(scanner.Text())
+	}
+	result := numIslands(grid)
+	fmt.Println(result)
+}
